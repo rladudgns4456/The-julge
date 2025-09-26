b78c82c7ce44cafe7471d49c0c2de7ad71cef1d8
rladudgns4456 <92049252+rladudgns4456@users.noreply.github.com>
2025-09-26 17:17:18 +0900

    [수정] 브랜치 최신화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center text-[50px]">찬민 테스트</div>
+      <div className="flex flex-col items-center justify-center text-[50px] bg-blue-200">찬민 테스트</div>
       <div className="text-[20px]">테일윈드 3.4버전 입니다</div>
     </>
   );
