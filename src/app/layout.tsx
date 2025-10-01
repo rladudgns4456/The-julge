2857068da88241f19f3d29002df25e6f508834b2
FE-kimwonseon <gws8221@gmail.com>
2025-10-01 17:01:24 +0900

    feat : Footer 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/common/header/Header";
+import Footer from "@/components/common/footer/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
