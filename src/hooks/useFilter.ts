8af13009a33d0b48626c63035eae971cdabd7862
DESKTOP-0JE7RRL\82105 <gowoonsorr@gmail.com>
2025-10-03 22:56:30 +0900

    [추가] 필터 상태 관리 훅(useFilter) 구현

---
+++ src/hooks/useFilter.ts
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { FilterOptions } from "@/types/filter";
+import { getTodayDate } from "@/utils/date";
+
+// 필터 상태관리 훅
+
+export const useFilter = (initialFilters: FilterOptions = {}) => {
+  const [filters, setFilters] = useState<FilterOptions>({
+    ...initialFilters,
+    startDate: initialFilters.startDate || getTodayDate(),
+  });
+
+  // 필터 값 변경 핸들러
+  const handleFilterChange = (key: keyof FilterOptions, value: string | number | string[] | Date | undefined) => {
+    setFilters(prev => ({
+      ...prev,
+      [key]: value || undefined,
+    }));
+  };
+
+  // 지역 선택 토글
+  const handleLocationToggle = (location: string) => {
+    const currentLocations = filters.location || [];
+
+    if (currentLocations.includes(location)) {
+      // 이미 선택된 지역이면 제거
+      const updatedLocations = currentLocations.filter(loc => loc !== location);
+      handleFilterChange("location", updatedLocations.length > 0 ? updatedLocations : undefined);
+    } else {
+      // 새로운 지역 추가
+      handleFilterChange("location", [...currentLocations, location]);
+    }
+  };
+
+  // 필터 초기화
+  const resetFilters = () => {
+    setFilters({
+      ...initialFilters,
+      startDate: initialFilters.startDate || getTodayDate(),
+    });
+  };
+
+  return {
+    filters,
+    handleFilterChange,
+    handleLocationToggle,
+    resetFilters,
+  };
+};
