be9e919d2b981f50c5fde3fa78991dbcd7a37a0b
FE-kimwonseon <gws8221@gmail.com>
2025-10-17 14:11:31 +0900

    [추가] 공고 목록 컴포넌트 추가

---
+++ src/components/owner/NoticeList.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from "next/navigation";
+import { PostData } from "@/types/post";
+import PostCard from "../common/post/PostCard";
+
+interface NoticeListProps {
+  notices: PostData[];
+}
+
+export const NoticeList = ({ notices }: NoticeListProps) => {
+  const router = useRouter();
+
+  const handleNoticeClick = (notice: PostData) => {
+    router.push(`/owner/notice/${notice.id}`);
+  };
+
+  return (
+    <div className="max-w-[964px] w-full mx-auto grid grid-cols-2 md:grid-cols-3 gap-4">
+      {notices.map(notice => (
+        <PostCard key={notice.id} post={notice} onClick={() => handleNoticeClick(notice)} />
+      ))}
+    </div>
+  );
+};
