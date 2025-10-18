6676c7a3b5fbadd0eab7f497d21e4ee219d7f26e
FE-kimwonseon <gws8221@gmail.com>
2025-10-19 02:00:03 +0900

    [수정] AlertsApi파일 에러 핸들러 추가

--- src/api/error/ErrorHandler.ts
+++ src/api/error/ErrorHandler.ts
@@ -7,8 +7,13 @@ interface ErrorMessage {
 export const handleApiError = (error: unknown): never => {
   const axiosError = error as AxiosError<ErrorMessage>;
 
-  if (axiosError.response) {
+  if (axiosError.response?.data?.message) {
     throw new Error(axiosError.response.data.message);
   }
+
+  if (axiosError.message) {
+    throw new Error(axiosError.message);
+  }
+
   throw new Error("서버를 연결할 수 없습니다. 인터넷 연결을 확인해주세요.");
 };
