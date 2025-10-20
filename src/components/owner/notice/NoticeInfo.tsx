2b8d1b3daf5feef11f61d10d346a95a8f1176312
ChanMinJeong <setcorin@naver.com>
2025-10-20 17:24:38 +0900

    [찬민] 리베이스하면서 남은 마커들 제거

--- src/components/owner/notice/NoticeInfo.tsx
+++ src/components/owner/notice/NoticeInfo.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import Button from "@/components/common/button";
 import { formatDate, formatTime } from "@/utils/date";
 import { NoticeDetailItem } from "@/types/notice";
-<<<<<<< HEAD
 
 interface NoticeInfoProps {
   noticeDetail: NoticeDetailItem;
@@ -12,18 +11,6 @@ interface NoticeInfoProps {
 
 export const NoticeInfo = ({ noticeDetail, onClick }: NoticeInfoProps) => {
   const { shop, hourlyPay, startsAt, workhour, description, id } = noticeDetail;
-=======
-import { UserType } from "@/types/user";
-
-interface NoticeInfoProps {
-  noticeDetail: NoticeDetailItem;
-  userRole?: UserType;
-  onClick?: () => void;
-}
-
-export const NoticeInfo = ({ noticeDetail, userRole = "employer", onClick }: NoticeInfoProps) => {
-  const { shop, hourlyPay, startsAt, workhour, description, closed } = noticeDetail;
->>>>>>> 9263d76 ([찬민]dev 최신화)
 
   // 숫자를 통화 형식으로 포멧팅
   const formatCurrency = (amoubt: number): string => {
@@ -105,15 +92,9 @@ export const NoticeInfo = ({ noticeDetail, userRole = "employer", onClick }: Not
             {/* 버튼 */}
             <div className="w-full flex gap-2">
               <div className="w-full">
-<<<<<<< HEAD
                 <Link href={`/owner/register-notice?mode=edit&shopId=${shop.item.id}&noticeId=${id}`}>
                   <Button variant="outlined" className=" w-full" size="large" disabled={closed}>
                     {closed ? "마감 완료" : "편집하기"}
-=======
-                <Link href={`/owner/edit-shop/${shop.item.id}`}>
-                  <Button variant="outlined" className=" w-full" size="large">
-                    편집하기
->>>>>>> 9263d76 ([찬민]dev 최신화)
                   </Button>
                 </Link>
               </div>
@@ -125,11 +106,7 @@ export const NoticeInfo = ({ noticeDetail, userRole = "employer", onClick }: Not
       {/* 공고 설명 */}
       <div className="w-full bg-gray-10 mt-6 p-8 rounded-xl">
         <span className="text-body-1-bold text-black mb-3">공고 설명</span>
-<<<<<<< HEAD
         <p className="text-body-1-regular line-clamp-3">{description}</p>
-=======
-        <p className="text-body-1-regular line-clamp-3">{shop.item.description}</p>
->>>>>>> 9263d76 ([찬민]dev 최신화)
       </div>
     </div>
   );
